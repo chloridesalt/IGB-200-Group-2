11e482cd865abca1fc1121af0d5a785b757715b9
chloridesalt <qarribilly@gmail.com>
2026-08-24 17:46:19 +1000

    CineMachine added camera Transition  added

--- Where paper grows/My project/Assets/Scripts/scr_GameManager.cs
+++ Where paper grows/My project/Assets/Scripts/scr_GameManager.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,6 +6,10 @@ public class GameManager : MonoBehaviour
     public static GameManager s_Instance { get; private set; }
     public Camera MainCamera;
     public GameObject Sun;
+    public CinemachineCamera VcamStart;
+    public CinemachineCamera VcamTarget;
+    public KeyCode triggerKey = KeyCode.Space;
+    public KeyCode triggerKey2 = KeyCode.P;
 
     private void Awake()
     {
@@ -18,6 +23,36 @@ public class GameManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject); 
     }
+     void Update()
+    {
+       // Used to test camera transition  
+        if (Input.GetKeyDown(triggerKey))
+        {
+            CameraTransitionTop();
+        }
+        if (Input.GetKeyDown(triggerKey2))
+        {
+            CameraTransitionBottom();
+        }
+
+
+    }
+    // Use this function to change the camera view to the top of the paper bag
+    public void CameraTransitionTop()
+
+    {
+        VcamStart.Priority = 5;
+        VcamTarget.Priority = 10;
+    }
+
+    // Use this function to change the camera view to the bottom of the paper bag
+    public void CameraTransitionBottom()
+
+    {
+        VcamStart.Priority = 10;
+        VcamTarget.Priority = 5;
+    }
+
 
 
 }
