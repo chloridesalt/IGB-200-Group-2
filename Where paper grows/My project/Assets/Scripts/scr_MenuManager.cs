8e00e2513bbe6625cb58195e1d4ce40eec368932
Jelolt <jleot1400@gmail.com>
2026-09-05 14:25:25 +1000

    Credits button and screen
    Added credits button, button highlighting and credits screen pop up

--- Where paper grows/My project/Assets/Scripts/scr_MenuManager.cs
+++ Where paper grows/My project/Assets/Scripts/scr_MenuManager.cs
@@ -3,9 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject creditsPanel;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
     }
 
     public void QuitGame()
