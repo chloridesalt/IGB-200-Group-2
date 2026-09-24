fdc1b3548340fa0981637d144c95e15cd0d825b1
chloridesalt <qarribilly@gmail.com>
2026-09-24 18:59:44 +1000

    Wind manager updates

--- Where paper grows/My project/Assets/Scripts/Environment/src_particleEmitter.cs
+++ Where paper grows/My project/Assets/Scripts/Environment/src_particleEmitter.cs
@@ -1,37 +1,65 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class src_ParticleEmitter : MonoBehaviour
 {
+    [Header("Random Spawning Settings")]
+    [SerializeField] private float minInterval = 0.2f; // Min time between leaf pops
+    [SerializeField] private float maxInterval = 0.8f; // Max time between leaf pops
+
     private ParticleSystem leafParticles;
-    private ParticleSystem.EmissionModule emissionModule;
+    private Coroutine emitRoutine;
 
     void Awake()
     {
         leafParticles = GetComponent<ParticleSystem>();
-        emissionModule = leafParticles.emission;
-        emissionModule.enabled = false; // Start disabled
+
+        // Turn off continuous emission rate so particles ONLY spawn via code
+        var emission = leafParticles.emission;
+        emission.rateOverTime = 0;
     }
 
     public void StartEmitting()
     {
-        // Ensure the particle system is actually running when emission turns on
+        StopEmitting();
+        emitRoutine = StartCoroutine(EmitLeavesRoutine());
+    }
+
+    public void StopEmitting()
+    {
+        if (emitRoutine != null)
+        {
+            StopCoroutine(emitRoutine);
+            emitRoutine = null;
+        }
+    }
+
+    private IEnumerator EmitLeavesRoutine()
+    {
         if (!leafParticles.isPlaying)
         {
             leafParticles.Play();
         }
 
-        emissionModule.enabled = true;
-    }
+        while (true)
+        {
+            //  Wait a random amount of time before popping out leaves
+            float randomWait = Random.Range(minInterval, maxInterval);
+            yield return new WaitForSeconds(randomWait);
 
-    public void StopEmitting()
-    {
-        emissionModule.enabled = false;
+            //  Randomly pick either 1 or 2 leaves (3 is exclusive in Random.Range for ints)
+            int leafAmount = Random.Range(1, 3);
+
+            // Emit exact count
+            leafParticles.Emit(leafAmount);
+        }
     }
 
     private void OnDisable()
     {
-        // Safety check when object is destroyed OR deactivated/pooled
+        StopEmitting();
+
         if (src_WindManager.Instance != null)
         {
             src_WindManager.Instance.UnregisterObject(this);
